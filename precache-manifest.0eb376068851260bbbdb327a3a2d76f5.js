6407e276eda3639dbe3a5b66ec2774b6a24a7366
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2019-04-08 08:02:04 +0000

    Updates

---
+++ precache-manifest.0eb376068851260bbbdb327a3a2d76f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6550e674febf2e8cf0",
+    "url": "./static/css/main.2a19975e.chunk.css"
+  },
+  {
+    "revision": "ef6550e674febf2e8cf0",
+    "url": "./static/js/main.49e56a40.chunk.js"
+  },
+  {
+    "revision": "17013fa50d1d1491f5aa",
+    "url": "./static/js/runtime~main.66bc1e24.js"
+  },
+  {
+    "revision": "be00c15ea04b2eca8353",
+    "url": "./static/js/2.2b54bb2a.chunk.js"
+  },
+  {
+    "revision": "de06e21d6cf0c705e8e9",
+    "url": "./static/js/3.c37c096b.chunk.js"
+  },
+  {
+    "revision": "f4707da0be2537792d2a",
+    "url": "./static/css/4.67126f2e.chunk.css"
+  },
+  {
+    "revision": "f4707da0be2537792d2a",
+    "url": "./static/js/4.09b2f102.chunk.js"
+  },
+  {
+    "revision": "9ae8235d7c04dc967a56f2495ec80a94",
+    "url": "./index.html"
+  }
+];
